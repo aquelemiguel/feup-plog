8c90f386410b66eb4f939263c4d42b01afb4ab41
zepedrob16 <ze_pedro_borges@hotmail.com>
2017-10-13 17:41:39 +0100

    gotta go fast

--- project-oolong/oolong.pl
+++ project-oolong/oolong.pl
@@ -48,6 +48,11 @@ print_formatted_line(X) :-
   nl, put_code(200), put_code(205), put_code(205), put_code(205), put_code(202), put_code(205), put_code(205), put_code(205), put_code(202), put_code(205),
   put_code(205), put_code(205), put_code(188), nl.
 
+print_formatted_line(X) :-
+  X = 3,
+  put_code(204), put_code(205), put_code(205), put_code(205), put_code(206), put_code(205), put_code(205), put_code(205), put_code(206), put_code(205),
+  put_code(205), put_code(205), put_code(185), write(' ').
+
 % This implementation looks like absolute shit.
 print_block([H|T]) :- nl, nl,
   nl, print_formatted_line(0), write(' '), print_formatted_line(0), write(' '), print_formatted_line(0), nl,
@@ -62,7 +67,7 @@ print_block([H|T]) :- nl, nl,
 
   nth0(0, A1, E4), write(' '), write(E4), write(' '), put_code(186),
   nth0(1, A1, E5), write(' '), write(E5), write(' '), put_code(186),
-  nth0(2, A1, E6), write(' '), write(E6), write(' '), put_code(186), write(' '), put_code(186),  
+  nth0(2, A1, E6), write(' '), write(E6), write(' '), put_code(186), write(' '), put_code(186),
 
   nth0(1, T, A2),
 
@@ -70,32 +75,44 @@ print_block([H|T]) :- nl, nl,
   nth0(1, A2, E8), write(' '), write(E8), write(' '), put_code(186),
   nth0(2, A2, E9), write(' '), write(E9), write(' '), put_code(186),
 
-  print_formatted_line(1), put_code(186),
+  nl,print_formatted_line(3), print_formatted_line(3), print_formatted_line(3), nl,
 
-  nth0(3, H, E10), write(E10), write(' '), put_code(186),
-  nth0(4, H, E11), write(E11), write(' '), put_code(186),
-  nth0(5, H, E12), write(E12), write('    '),
+  put_code(186),
 
-  nth0(3, A1, E13), write(E13), write('  '),
-  nth0(4, A1, E14), write(E14), write('  '),
-  nth0(5, A1, E15), write(E15), write('    '),
+  nth0(3, H, E10), write(' '), write(E10), write(' '), put_code(186),
+  nth0(4, H, E11), write(' '), write(E11), write(' '), put_code(186),
+  nth0(5, H, E12), write(' '), write(E12), write(' '), put_code(186), write(' '),
 
-  nth0(3, A2, E16), write(E16), write('  '),
-  nth0(4, A2, E17), write(E17), write('  '),
-  nth0(5, A2, E18), write(E18),
+  put_code(186), write(' '),
 
-  print_formatted_line(1), put_code(186),
+  nth0(3, A1, E13), write(E13), write(' '), put_code(186), write(' '),
+  nth0(4, A1, E14), write(E14), write(' '), put_code(186), write(' '),
+  nth0(5, A1, E15), write(E15), write(' '), put_code(186), write(' '),
 
-  nth0(6, H, E19), write(E19), write('  '),
-  nth0(7, H, E20), write(E20), write('  '),
-  nth0(8, H, E21), write(E21), write('    '),
+  put_code(186), write(' '),
 
-  nth0(6, A1, E22), write(E22), write('  '),
-  nth0(7, A1, E23), write(E23), write('  '),
-  nth0(8, A1, E24), write(E24), write('    '),
+  nth0(3, A2, E16), write(E16), write(' '), put_code(186), write(' '),
+  nth0(4, A2, E17), write(E17), write(' '), put_code(186), write(' '),
+  nth0(5, A2, E18), write(E18), write(' '), put_code(186), write(' '),
 
-  nth0(6, A2, E25), write(E25), write('  '),
-  nth0(7, A2, E26), write(E26), write('  '),
+  nl,print_formatted_line(3), print_formatted_line(3), print_formatted_line(3), nl,
+
+  put_code(186), write(' '),
+
+  nth0(6, H, E19), write(E19), write(' '), put_code(186), write(' '),
+  nth0(7, H, E20), write(E20), write(' '), put_code(186), write(' '),
+  nth0(8, H, E21), write(E21), write(' '), put_code(186), write(' '), put_code(186),
+
+  write(' '),
+
+  nth0(6, A1, E22), write(E22), write(' '), put_code(186), write(' '),
+  nth0(7, A1, E23), write(E23), write(' '), put_code(186), write(' '),
+  nth0(8, A1, E24), write(E24), write(' '), put_code(186), write(' '), put_code(186),
+
+  write(' '),
+
+  nth0(6, A2, E25), write(E25), write(' '), put_code(186), write(' '),
+  nth0(7, A2, E26), write(E26), write(' '), put_code(186), write(' '),
   nth0(8, A2, E27), write(E27), nl,
 
   print_formatted_line(2),
