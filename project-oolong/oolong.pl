5a832b644e6a0cc199d47f50a0df28c9912263f9
aquelemiguel <miguelmanofernandes@gmail.com>
2017-10-13 01:04:48 +0100

    Finished board but code's dog shit

--- project-oolong/oolong.pl
+++ project-oolong/oolong.pl
@@ -1,7 +1,7 @@
 :- use_module(library(lists)).
 
 play :-
-  write('Welcome to PrOolong.'), nl,
+  nl, write('Welcome to PrOolong.'), nl,
   % read(Input),
   game([
     [a, b, c, d, e, f, g, h, i],
@@ -19,15 +19,70 @@ play :-
     [b, g, b, g, x, g, g, g, x] % Tile majority tracker.
   ).
 
-trim(L,N,S) :- length(P,N), append(P,S,L).
+trim_head(L,N,S) :- length(P,N), append(P,S,L). % Trims head of list. L=list N=amount S=result P=sized list
+trim_tail(L,N,S) :- reverse(L,R), length(P,N), append(P,K,R), reverse(K,S).
 
 print_board([]).
 print_block([]).
-print_line([]).
 
 print_board([H|T]) :-
-  print_block([H|T]),
-  trim(T, 2, Remain), % Removes the top 9x3 block.
+  length([H|T], Matrix_Size),
+  Trim_Size is Matrix_Size - 3,
+  trim_tail([H|T], Trim_Size, Block),
+  print_block(Block),
+  trim_head(T, 2, Remain),
+  print_board(Remain).
+
+% This implementation looks like absolute shit.
+print_block([H|T]) :- nl,
+  nth0(0, H, E1), write(E1), write(' '),
+  nth0(1, H, E2), write(E2), write(' '),
+  nth0(2, H, E3), write(E3), write('  '),
+
+  nth0(0, T, A1),
+
+  nth0(0, A1, E4), write(E4), write(' '),
+  nth0(1, A1, E5), write(E5), write(' '),
+  nth0(2, A1, E6), write(E6), write('  '),
+
+  nth0(1, T, A2),
+
+  nth0(0, A2, E7), write(E7), write(' '),
+  nth0(1, A2, E8), write(E8), write(' '),
+  nth0(2, A2, E9), write(E9), nl,
+
+  nth0(3, H, E10), write(E10), write(' '),
+  nth0(4, H, E11), write(E11), write(' '),
+  nth0(5, H, E12), write(E12), write('  '),
+
+  nth0(3, A1, E13), write(E13), write(' '),
+  nth0(4, A1, E14), write(E14), write(' '),
+  nth0(5, A1, E15), write(E15), write('  '),
+
+  nth0(3, A2, E16), write(E16), write(' '),
+  nth0(4, A2, E17), write(E17), write(' '),
+  nth0(5, A2, E18), write(E18), nl,
+
+  nth0(6, H, E19), write(E19), write(' '),
+  nth0(7, H, E20), write(E20), write(' '),
+  nth0(8, H, E21), write(E21), write('  '),
+
+  nth0(6, A1, E22), write(E22), write(' '),
+  nth0(7, A1, E23), write(E23), write(' '),
+  nth0(8, A1, E24), write(E24), write('  '),
+
+  nth0(6, A2, E25), write(E25), write(' '),
+  nth0(7, A2, E26), write(E26), write(' '),
+  nth0(8, A2, E27), write(E27), nl, nl.
+
+
+
+/*
+print_board([H|T]) :-
+  length([H|T], Size), % Size of matrix.
+  trim_tail([H|T], Size, Block),
+  print_block(Block),
+  trim_head(T, 2, Remain), % Removes the top 9x3 block.
   print_board(Remain).
 
 print_block([H|T]) :-
@@ -50,7 +105,7 @@ print_line([H|T]) :-
   nth0(0, A2, E7), write(E7),
   nth0(1, A2, E8), write(E8),
   nth0(2, A2, E9), write(E9).
-
+*/
 
 % print_line([]) :- nl.
 % print_line([C|T]) :- write(C), write('|'), print_line(T).
