564fd077368ec940688cf8590f39acf55fdf6490
aquelemiguel <miguelmanofernandes@gmail.com>
2017-11-02 19:01:50 +0000

    Board updates based on input

--- project-oolong/game.pl
+++ project-oolong/game.pl
@@ -1,7 +1,7 @@
 init_game(Game, GameMode) :-
   empty_board(Board),
   special_actions(Special),
-  Game = [Board, Special, blackPlayer, GameMode],
+  Game = [Board, Special, b, 4, GameMode],
   start_game(Game).
 
 % Game class getters.
@@ -14,26 +14,37 @@ get_special(Game, Special) :-
 get_turn(Game, Player) :-
   nth0(2, Game, Player).
 
+get_table_index(Game, TableIndex) :-
+  nth0(3, Game, TableIndex).
+
 get_gamemode(Game, GameMode) :-
-  nth0(3, Game, GameMode).
+  nth0(4, Game, GameMode).
+
+/**
+  @desc Places the current player's piece on the provided seat.
+*/
+place_piece(Game, SeatIndex, UpdatedGame) :-
+  get_board(Game, Board),
+  get_table_index(Game, TableIndex),
+  get_turn(Game, Player),
+
+  nth0(TableIndex, Board, Table),
+
+  replace(Table, SeatIndex, Player, NewTable),
+  replace(Board, TableIndex, NewTable, NewBoard),
+
+  replace(Game, 0, NewBoard, UpdatedGame).
 
 % Switches current turn.
 switch_turn(Game, UpdatedGame) :-
   get_turn(Game, Turn),
-  Turn = greenPlayer,
-  replace(Game, 2, blackPlayer, UpdatedGame).
+  Turn = g,
+  replace(Game, 2, b, UpdatedGame).
 
 switch_turn(Game, Result) :-
   get_turn(Game, Turn),
-  Turn = blackPlayer,
-  replace(Game, 2, greenPlayer, UpdatedGame).
-
-% Player and pieces.
-player(greenPlayer).
-player(blackPlayer).
-
-piece(greenPiece).
-piece(blackPiece).
+  Turn = b,
+  replace(Game, 2, g, UpdatedGame).
 
 % Initial boards.
 empty_board([
--- project-oolong/oolong.pl
+++ project-oolong/oolong.pl
@@ -9,129 +9,48 @@ oolong :- main_menu. % Entry function call.
 
 start_game(Game) :-
   get_board(Game, Board),
-  switch_turn(Game, UpdatedGame),
-  play(Game).
-
-
-play(Game) :-
   print_board(Board),
   get_gamemode(Game, Mode), Mode = 1,
-  input_coords(table, position).
+  play_turn(Game, UpdatedGame),
+  start_game(UpdatedGame).
 
-getInt(Input):-
-	read(TempInput),
-	Input is TempInput - 48.
-
-input_coords(table, position) :-
-  getInt(table).
-
-  % Split Array into Args
-string_to_move(StringA-StringB, ListOfMoves):-
-    StringA is Table, StringB is Position,
-    ListOfMoves = [Table,Position].
-
-is_column(Letter) :- member(Letter, "abcdefghj").
-
-
-% TODO: Waiter should only be implemented when board's optimized.
-
-
-
-/*
-print_board([]).
-print_block([]).
-
-print_board([H|T]) :-
-  length([H|T], Matrix_Size),
-  Trim_Size is Matrix_Size - 3,
-  trim_tail([H|T], Trim_Size, Block),
-  print_block(Block),
-  trim_head(T, 2, Remain),
-  print_board(Remain).
-
-print_formatted_line(X) :-
-  X = 0,
-  put_code(201), put_code(205), put_code(205), put_code(205), put_code(203), put_code(205), put_code(205), put_code(205), put_code(203), put_code(205),
-  put_code(205), put_code(205), put_code(187).
-
-print_formatted_line(X) :-
-  X = 1,
-  nl, put_code(204), put_code(205), put_code(205), put_code(205), put_code(206), put_code(205), put_code(205), put_code(205), put_code(206), put_code(205),
-  put_code(205), put_code(205), put_code(185), nl.
-
-print_formatted_line(X) :-
-  X = 2,
-  put_code(200), put_code(205), put_code(205), put_code(205), put_code(202), put_code(205), put_code(205), put_code(205), put_code(202), put_code(205),
-  put_code(205), put_code(205), put_code(188).
-
-print_formatted_line(X) :-
-  X = 3,
-  put_code(204), put_code(205), put_code(205), put_code(205), put_code(206), put_code(205), put_code(205), put_code(205), put_code(206), put_code(205),
-  put_code(205), put_code(205), put_code(185), write(' ').
-
-% This implementation looks like absolute shit.
-print_block([H|T]) :- nl, nl,
-  nl, print_formatted_line(0), write(' '), print_formatted_line(0), write(' '), print_formatted_line(0), nl,
-
-  put_code(186), write(' '),
-
-  nth0(0, H, E1), write(E1), write(' '), put_code(186), write(' '),
-  nth0(1, H, E2), write(E2), write(' '), put_code(186), write(' '),
-  nth0(2, H, E3), write(E3), write(' '), put_code(186), write(' '), put_code(186),
-
-  nth0(0, T, A1),
-
-  nth0(0, A1, E4), write(' '), write(E4), write(' '), put_code(186),
-  nth0(1, A1, E5), write(' '), write(E5), write(' '), put_code(186),
-  nth0(2, A1, E6), write(' '), write(E6), write(' '), put_code(186), write(' '), put_code(186),
-
-  nth0(1, T, A2),
-
-  nth0(0, A2, E7), write(' '), write(E7), write(' '), put_code(186),
-  nth0(1, A2, E8), write(' '), write(E8), write(' '), put_code(186),
-  nth0(2, A2, E9), write(' '), write(E9), write(' '), put_code(186),
-
-  nl,print_formatted_line(3), print_formatted_line(3), print_formatted_line(3), nl,
-
-  put_code(186),
-
-  nth0(3, H, E10), write(' '), write(E10), write(' '), put_code(186),
-  nth0(4, H, E11), write(' '), write(E11), write(' '), put_code(186),
-  nth0(5, H, E12), write(' '), write(E12), write(' '), put_code(186), write(' '),
-
-  put_code(186), write(' '),
-
-  nth0(3, A1, E13), write(E13), write(' '), put_code(186), write(' '),
-  nth0(4, A1, E14), write(E14), write(' '), put_code(186), write(' '),
-  nth0(5, A1, E15), write(E15), write(' '), put_code(186), write(' '),
-
-  put_code(186), write(' '),
-
-  nth0(3, A2, E16), write(E16), write(' '), put_code(186), write(' '),
-  nth0(4, A2, E17), write(E17), write(' '), put_code(186), write(' '),
-  nth0(5, A2, E18), write(E18), write(' '), put_code(186), write(' '),
-
-  nl,print_formatted_line(3), print_formatted_line(3), print_formatted_line(3), nl,
-
-  put_code(186), write(' '),
-
-  nth0(6, H, E19), write(E19), write(' '), put_code(186), write(' '),
-  nth0(7, H, E20), write(E20), write(' '), put_code(186), write(' '),
-  nth0(8, H, E21), write(E21), write(' '), put_code(186), write(' '), put_code(186),
-
-  write(' '),
-
-  nth0(6, A1, E22), write(E22), write(' '), put_code(186), write(' '),
-  nth0(7, A1, E23), write(E23), write(' '), put_code(186), write(' '),
-  nth0(8, A1, E24), write(E24), write(' '), put_code(186), write(' '), put_code(186),
-
-  write(' '),
-
-  nth0(6, A2, E25), write(E25), write(' '), put_code(186), write(' '),
-  nth0(7, A2, E26), write(E26), write(' '), put_code(186), write(' '),
-  nth0(8, A2, E27), write(E27), write(' '), put_code(186),
-
-  nl, print_formatted_line(2), write(' '), print_formatted_line(2), write(' '), print_formatted_line(2),
-
-  nl.
+/**
+  @desc Prompts next position from current player.
 */
+play_turn(Game, UpdatedGame) :-
+  read(SeatSimpleIndex),
+  SeatIndex is SeatSimpleIndex - 1,
+  validate_move(Game, SeatIndex),
+  write('Play validated!'),
+  place_piece(Game, SeatIndex, UpdatedGame).
+
+play_turn(Game) :- play_turn(Game).
+
+/**
+  @desc Checks whether the inputted seat is either already occupied or out of bounds.
+*/
+validate_move(Game, SeatIndex) :-
+  SeatIndex >= 0, SeatIndex =< 8,
+
+  get_board(Game, Board),
+  get_table_index(Game, TableIndex),
+
+  nth0(TableIndex, Board, Table),
+  nth0(SeatIndex, Table, Seat),
+
+  Seat = x.
+
+validate_move(Game, SeatIndex) :-
+  (SeatIndex < 0; SeatIndex > 8),
+  write('Seat out of bounds!'), fail.
+
+validate_move(Game, SeatIndex) :-
+  
+  get_board(Game, Board),
+  get_table_index(Game, TableIndex),
+
+  nth0(TableIndex, Board, Table),
+  nth0(SeatIndex, Table, Seat),
+
+  Seat \= x,
+  write('Seat already occupied!'), fail.
